bb82e8d34a2dd6d30950c70f6c31632000e0d90a
mateosp <paezdm@uninorte.edu.co>
2025-05-15 11:16:01 -0500

    configurando vercel

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_Bqq1MT5K.mjs';
-export { renderers } from '../renderers.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/static'
 
 export default defineConfig({
   // ...
-  output:'server',
+  output:'static',
   adapter: vercel(),
   integrations: [tailwind()],
 });
